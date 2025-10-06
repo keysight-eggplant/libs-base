6575bf8410f8cdfd653f836a5f371f37bbe26091
williameveretteggplant <44709277+williameveretteggplant@users.noreply.github.com>
2025-10-06 15:02:27 -0600

    Update NSXMLNode.m

--- Source/NSXMLNode.m
+++ Source/NSXMLNode.m
@@ -1186,13 +1186,13 @@ execute_xpath(xmlNodePtr node, NSString *xpath_exp, NSDictionary *constants,
 
 - (id) copyWithZone: (NSZone*)zone
 {
-  NSXMLNode *c = [[self class] allocWithZone: zone];
   xmlNodePtr newNode = xmlCopyNode([self _node], 1); // make a deep copy
   if (newNode == NULL) {
   	NSLog(@"Error: failed to copy node: %d", [self kind]);
-	[c release];
 	return nil;
   }
+  NSXMLNode *c = [[self class] allocWithZone: zone];
+
   clearPrivatePointers(newNode);
 
   c = [c _initWithNode: newNode kind: internal->kind];
