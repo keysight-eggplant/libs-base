e1f6d4374b1edbcbe4f988bd1530a5bd6a1b3626
William Everett <william.everett@keysight.com>
2025-10-09 13:31:10 -0600

    Fix error

--- Source/NSXMLNode.m
+++ Source/NSXMLNode.m
@@ -1187,7 +1187,8 @@ execute_xpath(xmlNodePtr node, NSString *xpath_exp, NSDictionary *constants,
 - (id) copyWithZone: (NSZone*)zone
 {
   xmlNodePtr newNode = NULL;
-  if ([self _node]->type == XML_DTD_NODE)
+  xmlNodePtr theNode = [self _node];
+  if (theNode->type == XML_DTD_NODE)
     {
       newNode = xmlCopyDtd([self _node]);
     }
